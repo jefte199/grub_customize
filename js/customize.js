5604da2eccecd163157bf5e81d8b924878543bf1
Jefte <jefte199@gmail.com>
2024-11-11 08:14:36 -0300

    feat: init config download zip

--- js/customize.js
+++ js/customize.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('imageUpload').addEventListener('change', function (event) {
     const file = event.target.files[0];
@@ -8,6 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
       const imagePreview = document.getElementById('imagePreview');
       imagePreview.src = e.target.result;
       imagePreview.style.display = 'block'; // Mostra a imagem após o carregamento
+
+      // Criando o arquivo ZIP 
+      const zip = new JSZip();
+      const telaFolder = zip.folder("tela");
+      telaFolder.file('background.jpg', e.target.result.split(',')[1], { base64: true });
+
+      // Quando o arquivo ZIP for gerado, criar o link para download
+      document.getElementById('downloadZip').style.display = 'block';
+      document.getElementById('downloadZip').addEventListener('click', function () {
+        zip.generateAsync({ type: "blob" }).then(function (content) {
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(content);
+          link.download = 'BootArt.zip';
+          link.click();
+        });
+      });
     };
 
     if (file) {
@@ -15,3 +31,4 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
